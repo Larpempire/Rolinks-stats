8f21a23eb313f35fcab0e2368cb652e75f05651c
Larpempire <bebegotowork@gmail.com>
2026-08-02 15:22:43 +0300

    Add package.json for project dependencies and scripts

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "larp-empire-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.3",
+    "node-fetch": "^2.7.0",
+    "express": "^4.19.2",
+    "dotenv": "^16.4.5"
+  }
+}
